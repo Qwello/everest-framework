fe46a6c97132eb878bf579747ad6697330d1cfaf
Dima Dorezyuk <ddo@qwello.eu>
2024-02-12 12:39:30 +0100

    Add mockall feature

--- everestrs/everestrs/src/lib.rs
+++ everestrs/everestrs/src/lib.rs
@@ -13,8 +13,10 @@ use std::sync::Weak;
 use thiserror::Error;
 
 // Reexport everything so the clients can use it.
-pub use serde_json;
+pub use mockall;
+pub use mockall_double;
 pub use serde;
+pub use serde_json;
 
 #[derive(Error, Debug)]
 pub enum Error {
